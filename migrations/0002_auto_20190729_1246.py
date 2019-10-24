e0c2d54745bf5b08a7250483eb849091593a3a30
Mikhail Kormanowsky <kormanowsky@gmail.com>
2019-10-25 01:34:43 +0300

    Update 0002_auto_20190729_1246.py

--- migrations/0002_auto_20190729_1246.py
+++ migrations/0002_auto_20190729_1246.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('impressmap_jwt', '0001_initial'),
+        ('rxjwt', '0001_initial'),
     ]
 
     operations = [
