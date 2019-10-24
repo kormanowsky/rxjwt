09164adb3fbb4cc46eb46ba2cbe3b8d6a08c9a9b
Михаил Кормановский <kormanowsky@gmail.com>
2019-10-25 01:26:28 +0300

    Initial commit

---
+++ auth.py
@@ -0,0 +1,28 @@
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+
+from django.conf import settings
+from .token import UserAccessToken
+
+
+class JWTAuthentication(BaseAuthentication):
+
+    def authenticate(self, request):
+        auth_header = request.META.get("HTTP_AUTHORIZATION")
+        if auth_header is None:
+            return None
+        if not len(auth_header.split()):
+            raise AuthenticationFailed("Invalid authentication header.")
+        auth_type, auth_token = auth_header.split()
+        if auth_type != "Token":
+            raise AuthenticationFailed("Invalid authentication header.")
+        user_access_token = UserAccessToken(auth_token)
+        if not user_access_token.verify(settings.SECRET_KEY):
+            raise AuthenticationFailed("The token is either invalid or expired, or the token's"
+                                       " owner is not found.")
+        current_user = user_access_token.get_user()
+        if hasattr(current_user, "datetime_removed") and current_user.datetime_removed is not None:
+            raise AuthenticationFailed("The token is either invalid or expired, or the token's"
+                                       " owner is not found.")
+        current_user.save()
+        return current_user, str(user_access_token)
